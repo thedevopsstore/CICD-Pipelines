1630c888ffdab86a4849169c36ff42a88042119c
Sceva Jordan <sceva@Scevas-Air.attlocal.net>
2022-05-26 22:27:36 -0400

    deploy

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome! Please hit `/qod` API to get the quote of the day."))
+	w.Write([]byte("Welcome! Please hit path `/qod` API to get the quote of the day."))
 }
 
 func quoteOfTheDayHandler(client *redis.Client) http.HandlerFunc {
